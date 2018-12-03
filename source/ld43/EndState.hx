3acaae5ab93fd73ab1ae93df653fa6c2ab66a0a7
Christopher Roelofs <christopher.roelofs@gmail.com>
2018-12-03 17:24:43 -0500

    add how to play to end screen

--- source/ld43/EndState.hx
+++ source/ld43/EndState.hx
@@ -10,54 +10,51 @@ import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 import flixel.math.FlxMath;
 import flixel.util.FlxDestroyUtil;
+
 using flixel.util.FlxSpriteUtil;
+
 import openfl.Assets;
 import ld43.StartState;
-
 
 /**
  * A FlxState which can be used for the game's menu.
  */
-class EndState extends FlxState
-{
+class EndState extends FlxState {
 	var _btnRestart:FlxSprite;
-	var _btnExit:FlxSprite;
+	var _btnHowTo:FlxSprite;
+
 	public var score:Int;
+
 	var scoreTxt:FlxText;
 
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
-	override public function create():Void
-	{
-
+	override public function create():Void {
 		FlxG.mouse.visible = true;
 
-		var bgImage = new FlxSprite(0,0,Assets.getBitmapData("assets/images/endscreen.png"));
+		var bgImage = new FlxSprite(0, 0, Assets.getBitmapData("assets/images/endscreen.png"));
 		add(bgImage);
 
-		_btnRestart = new FlxSprite(280,200,Assets.getBitmapData("assets/images/playagain.png"));
+		_btnRestart = new FlxSprite(280, 200, Assets.getBitmapData("assets/images/playagain.png"));
 		add(_btnRestart);
 
-		scoreTxt = new FlxText(750,520, 1000);
-		scoreTxt.scrollFactor.set(0, 0); 
+		_btnHowTo = new FlxSprite(1050, 600, Assets.getBitmapData("assets/images/howto.png"));
+		add(_btnHowTo);
+
+		scoreTxt = new FlxText(750, 520, 1000);
+		scoreTxt.scrollFactor.set(0, 0);
 		scoreTxt.color = FlxColor.BLUE;
 		scoreTxt.size = 60;
 		scoreTxt.text = '' + score;
 		add(scoreTxt);
 
-
-		//_btnExit = new FlxSprite(800,800,Assets.getBitmapData("assets/images/exit.png"));
-		//add(_btnExit);
-		
 		// FlxG.sound.playMusic(AssetPaths.end_soundtrack__ogg, .5, true);
-		
+
 		super.create();
 	}
-	
 
-	private function clickRestart():Void
-	{
+	private function clickRestart():Void {
 		// FlxG.switchState(new StartState());
 
 		var mapState = new MapState();
@@ -65,35 +62,33 @@ class EndState extends FlxState
 		FlxG.switchState(mapState);
 	}
 
-	private function clickExit():Void
-	{
-		System.exit(0);
+	private function clickHowTo():Void {
+		var howToState = new HowToState();
+		FlxG.switchState(howToState);
 	}
 
-	override public function update(elapsed:Float):Void
-   {
-      super.update(elapsed);
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
 
-   		if (FlxG.mouse.overlaps(_btnRestart)) {
+		if (FlxG.mouse.overlaps(_btnRestart)) {
 			if (FlxG.mouse.justPressed) {
 				clickRestart();
 			}
 		}
 
-   }
+		if (FlxG.mouse.overlaps(_btnHowTo)) {
+			if (FlxG.mouse.justPressed) {
+				clickHowTo();
+			}
+		}
+	}
 
-
-	
-
-	
-	
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
-	override public function destroy():Void
-	{
+	override public function destroy():Void {
 		super.destroy();
-		//_txtTitle = FlxDestroyUtil.destroy(_txtTitle);
+		// _txtTitle = FlxDestroyUtil.destroy(_txtTitle);
 	}
 }
